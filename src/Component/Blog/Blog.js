853d79c52cfa8c98cdb359210b21cb9f530ccdfb
faisal-al-fatha <faysal313@gmail.com>
2022-09-29 20:37:48 +0600

    question answered in accordian

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -9,7 +9,9 @@ const Blog = () => {
                     How does react work?
                 </div>
                 <div className="collapse-content">
-                    <p><span className='font-semibold'>Answer:</span> </p>
+                    <p><span className='font-semibold'>Answer:</span> React is used to build rich graphical user interfaces (UIs). It is based on the concept of reusable components, which allows you to create complex UIs out of small isolated pieces of code called components </p>
+                    <p>React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+                    <p>React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
                 </div>
             </div>
             {/* Question 2 */}
